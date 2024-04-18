ba9d7279cfe0b1fbd0d78cebd4177b48590f983c
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-18 16:35:02 +0300

    Add multi file upload form

---
+++ src/components/FileUpload/MultiFileUpload.vue
@@ -0,0 +1,29 @@
+<template>
+ <div class="custom-file-container" data-upload-id="mySecondImage">
+  <div class="label-container"><label>Upload </label> <a href="javascript:;" class="custom-file-container__image-clear" title="Clear Image">×</a></div>
+  <label class="custom-file-container__custom-file">
+   <input type="file" class="custom-file-container__custom-file__custom-file-input" multiple />
+   <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+   <span class="custom-file-container__custom-file__custom-file-control ltr:pr-20 rtl:pl-20"></span>
+  </label>
+  <div class="custom-file-container__image-preview"></div>
+ </div>
+</template>
+
+<script lang="ts" setup>
+ import { onMounted } from 'vue';
+ import FileUploadWithPreview from 'file-upload-with-preview';
+ import 'file-upload-with-preview/dist/file-upload-with-preview.min.css';
+ import '@/assets/css/file-upload-with-preview.min.css';
+
+ onMounted(() => {
+  // multiple image upload
+  new FileUploadWithPreview('mySecondImage', {
+   images: {
+    baseImage: '/assets/images/file-preview.svg',
+    backgroundImage: '',
+   },
+   multiple: true,
+  });
+ });
+</script>
