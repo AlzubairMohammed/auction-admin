3b5f6b31c1f23b1689e9209bb78825d606dc4819
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-18 14:28:08 +0300

    Add single select input component

---
+++ src/components/Inputs/SingleSelectInput.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="mb-5">
+  <multiselect
+   v-model="input1"
+   :options="options"
+   class="custom-multiselect"
+   :searchable="false"
+   :preselect-first="true"
+   :allow-empty="false"
+   :disabled="true"
+   selected-label=""
+   select-label=""
+   deselect-label=""
+  ></multiselect>
+ </div>
+</template>
+<script>
+ export default {
+  data() {
+   return {
+    input1: null,
+    options: [
+     { value: 'option1', label: 'Option 1' },
+     { value: 'option2', label: 'Option 2' },
+     { value: 'option3', label: 'Option 3' },
+     // Add more options here
+    ],
+   };
+  },
+ };
+</script>
