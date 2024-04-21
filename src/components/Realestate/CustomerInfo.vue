4f7f6d9d6cdb7de17fd82641eb2854f9455b5a0e
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-21 08:47:02 +0300

    Add realestate add form

---
+++ src/components/Realestate/CustomerInfo.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+ import BasicInput from '@/components/Inputs/BasicInput.vue';
+</script>
+<template>
+ <form>
+  <div class="mb-5">
+   <div class="flex mb-5">
+    <BasicInput placeholder="اسم العميل" />
+   </div>
+   <div class="flex mb-5">
+    <BasicInput placeholder="رقم الهاتف" />
+   </div>
+   <div class="flex mb-5"></div>
+   <div class=""></div>
+  </div>
+ </form>
+</template>
