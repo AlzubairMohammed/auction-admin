952c7241a57a9c5b06dd66213c1240a6af2e33b2
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-22 09:33:07 +0300

    Add input type textarea

---
+++ src/components/Inputs/TextAreaInput.vue
@@ -0,0 +1,43 @@
+<template>
+ <div class="flex">
+  <div
+   class="bg-[#eee] flex justify-center items-center ltr:rounded-l-md rtl:rounded-r-md px-3 font-semibold border ltr:border-r-0 rtl:border-l-0 border-[#e0e6ed] dark:border-[#17263c] dark:bg-[#1b2e4b] whitespace-nowrap"
+  >
+   {{ label }}
+  </div>
+  <textarea
+   :required="required"
+   :placeholder="placeholder"
+   v-model="modelValue"
+   rows="4"
+   class="form-textarea ltr:rounded-l-none rtl:rounded-r-none"
+  ></textarea>
+ </div>
+</template>
+
+<script lang="ts" setup>
+ import { ref, computed } from 'vue';
+ const props = defineProps<{
+  label: string;
+  placeholder: string;
+  modelValue: {
+   type: [String, Number, Boolean, Array, Object];
+   default: '';
+  };
+  required: {
+   type: Boolean;
+   default: false;
+  };
+ }>();
+
+ const inputEl = ref<HTMLInputElement | null>(null);
+ const emit = defineEmits(['update:modelValue', 'setRef']);
+
+ const computedValue = computed({
+  get: () => props.modelValue,
+  set: (value) => {
+   emit('update:modelValue', value);
+  },
+ });
+ emit('setRef', inputEl.value);
+</script>
