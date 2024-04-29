2530f633d57a90aebf918c9359cef5ba72eeee90
AlzubairMohammed <dev.alzubair@gmail.com>
2024-04-29 08:08:28 +0300

    Enhnce realestat components form desgin in md size screen

--- src/components/Scans/RealestateComponents.vue
+++ src/components/Scans/RealestateComponents.vue
@@ -27,17 +27,22 @@
     <div v-for="item in formData" class="w-1/5 flex flex-wrap p-2">
      <input
       v-model="item.value"
-      id="btnRight"
       type="text"
       placeholder=""
-      class="form-input w-1/6 ltr:rounded-r-none rtl:rounded-l-none ltr:border-r-0 rtl:border-l-0 font-semibold border border-[#e0e6ed] cursor-pointer"
+      class="form-input w-1/6 lg:w-1/5 ltr:rounded-r-none rtl:rounded-l-none ltr:border-r-0 rtl:border-l-0 font-semibold border border-[#e0e6ed] cursor-pointer"
      />
-     <select v-model="item.id" class="border border-[#e0e6ed] w-4/6 rounded-none select-no-ring:focus" @change="setStyle" @focus="setStyle" ref="select">
+     <select
+      v-model="item.id"
+      class="border border-[#e0e6ed] w-4/6 lg:w-3/5 rounded-none select-no-ring:focus"
+      @change="setStyle"
+      @focus="setStyle"
+      ref="select"
+     >
       <option>اختار المكون</option>
       <option v-for="component in realestateComponents" :value="component.id">{{ component.name }}</option>
      </select>
      <div
-      class="bg-info w-1/6 text-white flex justify-center items-center ltr:rounded-r-md rtl:rounded-l-md px-3 font-semibold border ltr:border-l-0 rtl:border-r-0 border-info cursor-pointer"
+      class="bg-info text-lg w-1/6 lg:w-1/5 text-white flex justify-center items-center ltr:rounded-r-md rtl:rounded-l-md px-3 font-semibold border ltr:border-l-0 rtl:border-r-0 border-info cursor-pointer"
       @click="formData.splice(formData.indexOf(item), 1)"
      >
       <icon-trash-lines />
