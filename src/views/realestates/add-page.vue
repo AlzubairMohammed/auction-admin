65726a63fff475634de858571d1f7de8dbd8c58d
john <dev.alzubair@gmail.com>
2024-05-05 20:01:52 +0300

    Just for test somthing

--- src/views/realestates/add-page.vue
+++ src/views/realestates/add-page.vue
@@ -1,9 +1,6 @@
 <script setup>
- //  import AddForm from '@/components/Realestate/AddForm.vue';
+ import AddForm from '@/components/Realestate/AddForm.vue';
 </script>
 <template>
- <!-- <AddForm /> -->
- <div>
-  <h1>hello</h1>
- </div>
+ <AddForm />
 </template>
