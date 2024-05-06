6f9063242f9fe10023b8b42fb29696a6bc6ae513
john <dev.alzubair@gmail.com>
2024-05-06 05:03:04 +0300

    test

--- src/components/Realestate/AddForm.vue
+++ src/components/Realestate/AddForm.vue
@@ -48,6 +48,12 @@
       backButtonText="الرجوع"
       finishButtonText="إنهاء"
      >
+      <tab-content
+       title="بيانات العميل"
+       customIcon='<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><circle cx="9" cy="9" r="2"/><path d="M13 15c0 1.105 0 2-4 2s-4-.895-4-2s1.79-2 4-2s4 .895 4 2Z"/><path d="M2 12c0-3.771 0-5.657 1.172-6.828C4.343 4 6.229 4 10 4h4c3.771 0 5.657 0 6.828 1.172C22 6.343 22 8.229 22 12c0 3.771 0 5.657-1.172 6.828C19.657 20 17.771 20 14 20h-4c-3.771 0-5.657 0-6.828-1.172C2 17.657 2 15.771 2 12Z" opacity="0.5"/><path stroke-linecap="round" d="M19 12h-4m4-3h-5"/><path stroke-linecap="round" d="M19 15h-3" opacity="0.9"/></g></svg>'
+      >
+       <CustomerInfo />
+      </tab-content>
      </form-wizard>
     </div>
    </div>
