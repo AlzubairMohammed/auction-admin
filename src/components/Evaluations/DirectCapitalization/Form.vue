fa7319834d1f9b2161cb63ed8078169ae9ad07f1
john <dev.alzubair@gmail.com>
2024-05-07 11:49:07 +0300

    Enhance direct capitalization form

--- src/components/Evaluations/DirectCapitalization/Form.vue
+++ src/components/Evaluations/DirectCapitalization/Form.vue
@@ -20,12 +20,15 @@
  <SelectRealestateModal v-model="isSelectRealestateModalActive" />
  <ResultModal v-model="isResultModalActive" :result="result" />
  <form @submit.prevent="onSubmit" class="mb-5 grid grid-cols-1 p-[100px] gap-5 p-5 bg-white shadow-md rounded-md">
+  <h1 class="text-2xl font-bold mb-5 text-center">
+   <span class="underline">الرسملة المباشرة</span>
+  </h1>
   <div class="flex flex-col space-y-4">
    <BasicInput v-model="form.cross_income" type="number" required placeholder="اجمالي الدخل للعقار" />
    <BasicInput v-model="form.operation_income_rate" placeholder="نسبة مصاريف التشغيل و الصيانة" type="number" />
    <BasicInput v-model="form.capitalization_rate" placeholder="معدل الرسملة (%)" type="number" />
   </div>
 
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" type="submit">تقييم</button>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 w-1/6" type="submit">تقييم</button>
  </form>
 </template>
